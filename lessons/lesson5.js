db5d67ba02b204b7744ccda55e786e5057e03b00
Jéssica Luz <qae.jessicaluz@gmail.com>
2024-04-14 20:03:00 +0100

    Lesson 5 -  Relational and equality comparators

---
+++ lessons/lesson5.js
@@ -0,0 +1,19 @@
+//Relational operators
+
+/* 
+    > more than
+    < less than
+    >= more than equal
+    <= less than equal 
+*/
+
+var result = 10 > 5;
+
+console.log (result);
+
+//Equality operators
+
+var x = 1;
+console.log( x == "1"); //loose comparison, means it compares the value
+console.log( x === 1 ); //stric comparison, means it compares the data and the typeconsole.log( x === 1 ); //stric comparison, means it compares the data and the type
+console.log( x === "1" ); //it's false because, even the value of x is 1, the type is number and not string
