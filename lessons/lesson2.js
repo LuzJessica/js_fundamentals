8c60fab6eb9d55b335248fe3a41680d2d7659573
Jéssica Luz <qae.jessicaluz@gmail.com>
2024-04-14 19:11:04 +0100

    Lesson 2 - Variables, Constants and Data Types

---
+++ lessons/lesson2.js
@@ -0,0 +1,31 @@
+//Variables 
+
+var firstName = "Jéssica"; //var 
+let lastName = "Luz";
+
+console.log(firstName + " " + lastName);
+
+var age, dateOfBirth, sex;
+
+age = 30;
+dateOfBirth = "27/05/1992";
+sex = "female";
+
+console.log ("Age: " + age);
+console.log ("Date of Birth: " + dateOfBirth);
+console.log ("Sex: " + sex);
+
+
+age = 31; //overwriting first value because its a var
+
+const occupation = "Computer Engineer";
+
+console.log("Occupation: " + occupation);
+
+//occupation = "QA"; this line cause compilation error because we are trying to overwritten the value of a const
+
+var middleName = "Cristina"; //string
+var ageOfSister = 31; //number
+var isMarried = false; //boolean
+var yearsOfMarriage = null; //null
+var numberOfPets = undefined; //undefined
